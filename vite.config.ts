27769ff7f195865032343fb04ba758716a62019b
alex8023 <alex8023@yandex.ru>
2025-11-23 17:37:34 +0300

    upd base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:
+    process.env.NODE_ENV === 'production' ? '/digital-solutions-front/' : '/',
 });
